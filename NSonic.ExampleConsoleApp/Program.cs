826ce0d862d4591db9256229862cb7066b448a5d
RayMMond <raymodsyp@gmail.com>
2020-06-10 11:04:31 +0800

    Test stream reader using example console app.

--- NSonic.ExampleConsoleApp/Program.cs
+++ NSonic.ExampleConsoleApp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace NSonic.ExampleConsoleApp
@@ -9,78 +12,23 @@ namespace NSonic.ExampleConsoleApp
         static async Task Main(string[] args)
         {
             var hostname = "localhost";
-            var port = 1491;
-            var secret = "SecretPassword";
+            var port = 18111;
+            var secret = "Secret";
+
+            
+            var task = StartListeningAsync(port, false); //change delay to false to trigger IOException
 
             try
             {
-                using (var control = NSonicFactory.Control(hostname, port, secret))
-                {
-                    control.Connect();
-
-                    var info = control.Info();
-                    Console.WriteLine($"INFO: {info}");
-
-                    info = await control.InfoAsync();
-                    Console.WriteLine($"INFO (async): {info}");
-
-                    control.Trigger("consolidate");
-                }
-
                 using (var search = NSonicFactory.Search(hostname, port, secret))
                 {
                     await search.ConnectAsync();
 
                     var queryResults = search.Query("messages", "user:1", "s");
                     Console.WriteLine($"QUERY: {string.Join(", ", queryResults)}");
-
-                    queryResults = await search.QueryAsync("messages", "user:1", "s");
-                    Console.WriteLine($"QUERY (async): {string.Join(", ", queryResults)}");
-
-                    var suggestResults = search.Suggest("messages", "user:1", "s");
-                    Console.WriteLine($"SUGGEST: {string.Join(", ", suggestResults)}");
-
-                    suggestResults = await search.SuggestAsync("messages", "user:1", "s");
-                    Console.WriteLine($"SUGGEST (async): {string.Join(", ", suggestResults)}");
                 }
 
-                using (var ingest = NSonicFactory.Ingest(hostname, port, secret))
-                {
-                    ingest.Connect();
-
-                    ingest.Push("messages", "user:1", "conversation:1", "This is an example push.", locale: null);
-
-                    var popResult = ingest.Pop("messages", "user:1", "conversation:1", "This is an example push.");
-                    Console.WriteLine($"POP: {popResult}");
-
-                    popResult = await ingest.PopAsync("messages", "user:1", "conversation:1", "This is an example push.");
-                    Console.WriteLine($"POP (async): {popResult}");
-
-                    var countResult = ingest.Count("messages", "user:1");
-                    Console.WriteLine($"COUNT: {countResult}");
-
-                    countResult = await ingest.CountAsync("messages", "user:1");
-                    Console.WriteLine($"COUNT (async): {countResult}");
-
-                    var flushCollectionResult = ingest.FlushCollection("messages");
-                    Console.WriteLine($"FLUSHC: {flushCollectionResult}");
-
-                    flushCollectionResult = await ingest.FlushCollectionAsync("messages");
-                    Console.WriteLine($"FLUSHC (async): {flushCollectionResult}");
-
-                    var flushBucketResult = ingest.FlushBucket("messages", "user:1");
-                    Console.WriteLine($"FLUSHB: {flushBucketResult}");
-
-                    flushBucketResult = await ingest.FlushBucketAsync("messages", "user:1");
-                    Console.WriteLine($"FLUSHB (async): {flushBucketResult}");
-
-                    var flushObjectResult = ingest.FlushObject("messages", "user:1", "conversation:1");
-                    Console.WriteLine($"FLUSHO: {flushObjectResult}");
-
-                    flushObjectResult = await ingest.FlushObjectAsync("messages", "user:1", "conversation:1");
-                    Console.WriteLine($"FLUSHO (async): {flushObjectResult}");
-                }
-
+                await task;
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey(true);
             }
@@ -88,6 +36,63 @@ namespace NSonic.ExampleConsoleApp
             {
                 Debug.WriteLine(e.ToString());
             }
+        }
+
+        static async Task StartListeningAsync(int port, bool delay)
+        {
+            await Task.Yield();
+
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine($"Start listening on {IPAddress.Any}:{port}.");
+
+            using var client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine($"Connected {client.Client.RemoteEndPoint}.");
+
+            using var reader = new StreamReader(client.GetStream());
+            using var writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+
+            await writer.WriteLineAsync("CONNECTED <sonic-server v1.2.3>");
+
+            while (true)
+            {
+                var line = await reader.ReadLineAsync();
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    break;
+                }
+
+                if (line.Contains("QUIT", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    await writer.WriteLineAsync("ENDED quit");
+                    break;
+                }
+
+                Console.WriteLine($"Received:{line}.");
+
+                if (line.StartsWith("START"))
+                {
+                    await writer.WriteLineAsync("STARTED search protocol(1) buffer(20000)");
+                }
+                else if (line.StartsWith("QUERY"))
+                {
+                    if (delay)
+                    {
+                        await writer.WriteLineAsync("PENDING Bt2m2gYa");
+                        await Task.Delay(500);
+                        await writer.WriteLineAsync("EVENT QUERY Bt2m2gYa conversation:71f3d63b");
+                    }
+                    else
+                    {
+                        await writer.WriteAsync("PENDING Bt2m2gYa\r\nEVENT QUERY Bt2m2gYa conversation:71f3d63b\r\n");
+                    }
+                }
+            }
+
+            client.Close();
+            listener.Stop();
+            Console.WriteLine("Stop listening.");
         }
     }
 }
